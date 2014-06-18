32ef5c9e32ceb8e16c05bb7b0c32ad9946fe176d
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2014-06-18 01:30:44 -0500

    Switched database to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,6 @@ ActiveRecord::Schema.define(version: 20140616021129) do
     t.integer  "total_goals"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
